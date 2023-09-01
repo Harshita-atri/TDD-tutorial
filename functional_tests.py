30dc1d50ca5662e7f00bf1bbae6e7ebf951442d1
Naga Harshita Marupaka <marupaka@atrihub.io>
2023-09-01 11:24:52 -0700

    feat(tdd_training): Chapter 4 complete

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,9 @@
-import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -16,7 +20,23 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
 
-        # She is invited to enter a to-do item straight away
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
+
+
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+
+        inputbox.send_keys("Buy peacock feathers")
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows),
+                        "New to-do item did not appear in table",)
+
+
         self.fail("Finish the test!")
 
 
--- lists/tests.py
+++ lists/tests.py
@@ -3,9 +3,7 @@ from lists.views import home_page
 from django.http import HttpRequest
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
+
+    def test_uses_home_template(self):
         response = self.client.get("/")
-        html = response.content.decode("utf8")
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
+        self.assertTemplateUsed(response, "home.html")
