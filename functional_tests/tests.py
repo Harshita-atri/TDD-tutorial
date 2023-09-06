b82d02d5c5e89bcb84590cdc5a5869c54fe0cdb4
Naga Harshita Marupaka <marupaka@atrihub.io>
2023-09-06 07:46:13 -0700

    feat(tdd_training): Chapter 6 complete

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,10 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 import unittest
 
+MAX_WAIT = 5
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -17,12 +20,22 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements(By.TAG_NAME, "tr")
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_todo_list(self):
-        # Edith has heard about a cool new online to-do app.
-        # She goes to check out its homepage
-        self.browser.get("http://localhost:8000")
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
 
-        # She notices the page title and header mention to-do lists
+    def test_can_start_a_todo_list(self):
+        self.browser.get(self.live_server_url)
+
         self.assertIn("To-Do", self.browser.title)
 
         header_text = self.browser.find_element(By.TAG_NAME, "h1").text
@@ -34,15 +47,11 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
-
-if __name__ == "__main__":
-    unittest.main()
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
